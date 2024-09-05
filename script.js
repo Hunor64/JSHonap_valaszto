d75923e7888d6dec71369e3839b7a4e902f33eb3
hunor64 <nhunor06@gmail.com>
2024-09-05 09:09:36 +0200

    Added basic js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+let mothLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let selectedMonth = 0
+
+function GenerateMothTables() {
+    let mainDiv = document.querySelector(".days")
+    mainDiv.innerHTML = ""
+  if (document.querySelector(".month").value != "") {
+    selectedMonth = document.querySelector(".month").value
+    let length = mothLength[selectedMonth]
+    for (let index = 1; index < length; index++) {
+        let row = document.createElement("div")
+        let dayNumber = document.createElement("div")
+        dayNumber.innerHTML = index
+
+        let inputField = document.createElement("input")
+
+        row.appendChild(dayNumber)
+        row.appendChild(inputField)
+        mainDiv.appendChild(row)
+    }
+  }
+}
